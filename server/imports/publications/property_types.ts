0ac6a2adff5c1c003399fcf634cdbb55b771bafa
Elvir Tabakovic <elvir@claireit.nl>
2018-10-13 00:31:17 +0200

    typo fix

--- server/imports/publications/property_types.ts
+++ server/imports/publications/property_types.ts
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { IPropertyType } from "../../../imports/models/index";
 import { PropertyTypes } from "../../../imports/collections/index";
 
-Meteor.publish('proeprty-types', function (): Mongo.Cursor<IPropertyType> {
+Meteor.publish('property-types', function (): Mongo.Cursor<IPropertyType> {
     //if (!this.userId || !("role" in Meteor.user() && Meteor.user().role == "admin" )) {
     if (!this.userId) {
         return;
